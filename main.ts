e47af7f7704c771ef6a5775e53669eba925fd9e5
Arturo Hermosa <keyblademaster_01@hotmail.com>
2022-07-05 16:12:15 +0200

    Deleted solution, will add tutorial

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
 input.onSound(DetectedSound.Loud, function () {
-    for (let index = 0; index <= 4; index++) {
-        basic.showNumber(index + 1)
-        music.playTone(262, music.beat(BeatFraction.Whole))
-        music.stopAllSounds()
-    }
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.stopAllSounds()
 })
 basic.showIcon(IconNames.Duck)
 for (let index = 0; index < 2; index++) {
